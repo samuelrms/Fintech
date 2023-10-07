32f872fb204506415b7f4e2215bac7093f8b817c
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2023-10-07 12:15:06 -0300

    fix: variants stack

--- src/Components/Stack/Stack.types.ts
+++ src/Components/Stack/Stack.types.ts
@@ -1,6 +1,12 @@
 import { ComponentProps } from 'react'
 
-export type ElementsVariants = 'div' | 'aside' | 'article' | 'form'
+export type ElementsVariants =
+  | 'div'
+  | 'aside'
+  | 'article'
+  | 'form'
+  | 'section'
+  | 'span'
 
 export type Props = {
   /** Defines the HTML element that will be rendered. It can be a valid HTML element or a React component. */
